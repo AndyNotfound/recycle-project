621cb252f254b9f04cb27986058b9b5d2c2f2f74
kiki <muhammad.210170181@mhs.unimal.ac.id>
2023-10-14 11:28:24 +0700

    membuat halaman pickup

---
+++ pages/pickup.js
@@ -0,0 +1,11 @@
+import Navbar from '@/components/navbar'
+import Card from '@/components/pickup/sampah'
+
+export default function Pickup() {
+    return (
+        <main>
+            <Navbar />
+            <Card />
+        </main >
+    )
+}
